0afc8d26069e9207974a4206daf7dbc7aae5ff6e
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-05-10 12:35:02 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (70.47%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -6,13 +6,13 @@ class Solution {
         else if(n == 1){
             return true;
         }else{
-            while(n%2 == 0){
+            while(n%2 == 0){ // if the n = 20 then it will divide that number and 20/2=10 10/2=5 then 5/2=remainder 1 then i will flase 
                 n = n/2;
             }
             if(n == 1){
                 return true;
             }else{
-                return false;
+                return false; // it will come here and because 5 == 1 not true so it returnb false
             }
         }
         
