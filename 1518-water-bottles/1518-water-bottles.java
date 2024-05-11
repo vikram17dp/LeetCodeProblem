00b19218f7c1fcae2b88d02e1ef7588fe8e41bd2
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-05-11 16:53:23 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (59.84%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.java
+++ 1518-water-bottles/1518-water-bottles.java
@@ -1,11 +1,12 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-        int ans = numBottles;
-        while(numBottles>=numExchange){
-            int newBottles = numBottles / numExchange;
-            int remBottles = numBottles % numExchange;
-            ans = ans + newBottles;
-            numBottles = newBottles + remBottles;
+        // taken 2nd example and dry run the code
+        int ans = numBottles; //15
+        while(numBottles>=numExchange){ // 15>=4
+            int newBottles = numBottles / numExchange; // => 3
+            int remBottles = numBottles % numExchange; // =>3
+            ans = ans + newBottles; // => 15 + 3
+            numBottles = newBottles + remBottles; // 3 + 3 = 6 and finaly it will move on to the ans = numbootle and again loop will work.
         }
         return ans;
     }
