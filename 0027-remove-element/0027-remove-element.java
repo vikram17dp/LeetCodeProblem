e7e91ad50b359da6089d9d20c57ce4cd854b1af5
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-05-13 00:13:59 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (39.76%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int ptr=0;
+        for(int i=0;i<nums.length;i++){
+// if the nums[i] = val then it will consider as flase then it will comes out if not eqaul then it will return the nums[i] in ana array
+            if(nums[i]!=val){
+                nums[ptr] = nums[i];
+                ptr++;
+            }
+        }
+        return ptr;
+       
+    }
+   
+}
