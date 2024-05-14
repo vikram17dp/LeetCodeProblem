eb20ddad59ce939c441bad94adc6936d4d4694bc
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-05-15 00:42:31 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (23.09%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.java
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -2,6 +2,8 @@ class Solution {
     public int bulbSwitch(int n) {
         int i=1;
         int count =0;
+        // in an interview they are asking the logic in this question
+        // for every even numbers= bulb is OFF ans odd numbers = bulb is ON
         while(i*i <=n){
             count++;
             i++;
