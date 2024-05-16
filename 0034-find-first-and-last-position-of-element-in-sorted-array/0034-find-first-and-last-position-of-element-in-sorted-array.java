4a24369c3451c6bcdf1a94ec64dbd60b36ed5ea8
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-05-17 01:18:31 +0530

    Time: 0 ms (100.00%), Space: 45.6 MB (86.60%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) { // TC => O(logn)
+        int[] ans = {-1,-1};
+        if(nums.length == 0){ // if the number length is zero then it will -1,-1
+            return ans;
+        }
+        ans[0]=FirstOcurrance(nums,target); // if only when one index is their on that it will call same as last next one 
+        ans[1]=LastOcuurance(nums,target);
+        return ans;
+       
+    }
+    public int FirstOcurrance(int[] nums,int target){
+        int start = 0;
+        int end = nums.length-1;
+        int ans = -1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(nums[mid] == target){
+                ans = mid;
+                end = mid-1;
+            }else if(target>nums[mid]){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }  
+        }
+        return ans;
+       
+    }
+    public int LastOcuurance(int[] nums,int target){
+        int start = 0;
+        int end = nums.length-1;
+        int ans = -1;
+        while(start<=end){
+            int mid = (start + end)/2;
+            if(nums[mid] == target){
+                ans = mid;
+                start = mid+1;
+            }else if(target> nums[mid]){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+}
