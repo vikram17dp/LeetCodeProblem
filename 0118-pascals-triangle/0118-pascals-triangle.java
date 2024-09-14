abbd876a35d3f3723f055107e30d228418249e65
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-09-14 23:22:59 +0530

    Time: 1 ms (82.66%), Space: 41.9 MB (44.52%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,28 @@
+class Solution { // TC is O(N)
+    
+    public int nCr(int n,int r){
+         long res = 1;
+        
+        for(int i = 0;i<r;i++){
+            res = res*(n-i)/(i+1);
+            
+        }
+        return (int) res;
+    }
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        
+        for(int n = 0;n<numRows;n++){
+            List<Integer> row = new ArrayList<>();
+            
+        
+        for(int r =0;r<=n;r++){
+            row.add(nCr(n,r));
+        }
+        triangle.add(row);
+    }
+        return triangle;
+}
+    
+    
+}
