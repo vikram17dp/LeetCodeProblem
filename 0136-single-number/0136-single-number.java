b65e2f4d009922c53383841894746cac223af278
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-09-16 19:53:01 +0530

    Time: 14 ms (14.47%), Space: 44.5 MB (98.38%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int n = nums.length;
-        int count = 0;
-        for(int i =0;i<n;i++){
-           int num = nums[i];
-            count = 0;
-            for(int j =0;j<n;j++){
-                if(nums[j] == num){
-                    count++;
-                }
+        HashMap<Integer,Integer> hash = new HashMap<>();
+        for(int num:nums){
+            hash.put(num,hash.getOrDefault(num,0)+1);
+        }
+        
+        for(int num:hash.keySet()){
+            if(hash.get(num) == 1){
+                return num;
             }
-            if(count == 1) return num;
         }
         throw new IllegalArgumentException("No single number found");
         
