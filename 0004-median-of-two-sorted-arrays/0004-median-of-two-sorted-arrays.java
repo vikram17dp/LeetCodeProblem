0def1362e8653050ea0f9df300bb493ed2254eae
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-11-17 12:56:43 +0530

    Time: 2 ms (59.39%), Space: 45.7 MB (90.44%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -6,6 +6,11 @@ class Solution {
         int n2 = nums2.length;
         List<Integer> nums3 = new ArrayList<>();
         int i = 0,j = 0;
+        // two array are empty
+        if(n1 == 0 && n2 == 0){
+            return 0;
+        }
+        // two array having the elements
           while (i < n1 && j < n2) {
             if (nums1[i] < nums2[j]) {
                 nums3.add(nums1[i++]);
@@ -13,13 +18,16 @@ class Solution {
                 nums3.add(nums2[j++]);
             }
         }
+        // either one of having the empty array
         while(i<n1)  nums3.add(nums1[i++]);
         while(j<n2)  nums3.add(nums2[j++]);
+        
         int n = nums3.size();
+        
         if(n % 2 == 1){
-            return nums3.get(n/2);
+            return nums3.get(n/2); // for odd condition
         }else{
-            return (nums3.get(n/2) + nums3.get(n/2-1))/2.0;
+            return (nums3.get(n/2) + nums3.get(n/2-1))/2.0; // even condition
         }
     }
 }
