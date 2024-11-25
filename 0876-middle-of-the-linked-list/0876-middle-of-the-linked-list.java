63f5fb85cc3694238e8fa1eed53cddaec1472003
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-11-26 00:24:29 +0530

    Time: 0 ms (100.00%), Space: 41 MB (61.47%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -10,23 +10,35 @@
  */
 class Solution { // TC is O(n+n/2) and sc is O(1)
     public ListNode middleNode(ListNode head) {
-        int count = 0;
-        ListNode temp = head;
-        // step 1
-        while(temp != null){
-            count++;
-            temp = temp.next;
-        }
-        // step 2
+        // method one
+//         int count = 0;
+//         ListNode temp = head;
+//         // step 1
+//         while(temp != null){
+//             count++;
+//             temp = temp.next;
+//         }
+//         // step 2
         
-        int middleNode = (count/2)+1;
-        temp = head;
-        while(temp != null){
+//         int middleNode = (count/2)+1;
+//         temp = head;
+//         while(temp != null){
             
-            middleNode = middleNode -1;
-            if(middleNode == 0) break;
-            temp = temp.next;
+//             middleNode = middleNode -1;
+//             if(middleNode == 0) break;
+//             temp = temp.next;
+//         }
+//         return temp;
+        
+        // method two with slow and fast
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return temp;
+        return slow;
+        
+        
     }
 }
