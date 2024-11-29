4818c8b5ad2b6d2fdcd991ca744722a0fe08de91
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-11-29 22:35:39 +0530

    Time: 31 ms (44.89%), Space: 45.3 MB (35.02%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -6,7 +6,7 @@ class Solution {
         // update the map with current letters
         for(int i =0;i<str.length();i++){
             char c = str.charAt(i);
-            int freq = map.getOrDefault(c,0);
+            int freq = map.getOrDefault(c,0);// it will count the unique strings
             map.put(c,(freq+1));
         }
         // getting the unique string from the hashmap
