a168e54fb63a49e3cd302ffebe20c6b8f939a19b
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-11-30 22:07:51 +0530

    Time: 7 ms (37.53%), Space: 42.9 MB (80.99%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isAnagram(String str1, String str2) {
+        // converting all the strings into the lowercase
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        
+        // removing the whitespaces
+        str1 = str1.replace(" ","");
+        str2 = str2.replace(" ","");
+        
+        // intailze the bucket array of 26 
+        
+        int[] counts = new int[26];
+        
+        //fill the bucktes
+        for(int i = 0;i<str1.length();i++){
+            counts[str1.charAt(i)-'a']++;
+        }
+        
+        // empty the buckets
+        for(int i = 0;i<str2.length();i++){
+            counts[str2.charAt(i)-'a']--;
+        }
+        // apply the for each loop to travse entrie array
+        
+        for(int count:counts){
+            if(count!=0){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+}
