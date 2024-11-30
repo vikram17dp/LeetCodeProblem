58be60beece1e3b146e5fe2d9ef6b2175f911aa6
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-11-30 23:26:56 +0530

    Time: 15 ms (25.69%), Space: 48.1 MB (23.82%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String getfrequencyString(String str){
+        // creating an empty buckets
+        int[] freq = new int[26];
+        
+        // iterate over each charcter
+        for(char c:str.toCharArray()){
+            freq[c-'a']++;
+        }
+        // start crating the frequency string
+        StringBuilder frequencyString = new StringBuilder("");
+        char c = 'a';
+        
+        for(int i :freq){
+            frequencyString.append(c);
+            frequencyString.append(i);
+            c++;
+        }
+        return frequencyString.toString();
+    }
+    public List<List<String>> groupAnagrams(String[] str) {
+        // check for the empty inputs
+        if(str == null || str.length == 0) return new ArrayList<>();
+        
+        HashMap<String,List<String>> frequencyStringMap = new HashMap<>();
+        for(String strs :str){
+            String frequencyString = getfrequencyString(strs);
+            
+            // if the frequcny string is prsent,add the string to the list
+            if(frequencyStringMap.containsKey(frequencyString)){
+                frequencyStringMap.get(frequencyString).add(strs);
+            }
+            else{
+                // else create a new list
+                
+                List<String> strList = new ArrayList<>();
+                
+                strList.add(strs);
+                frequencyStringMap.put(frequencyString,strList);
+            }
+        }
+        return new ArrayList<>(frequencyStringMap.values());
+    }
+}
