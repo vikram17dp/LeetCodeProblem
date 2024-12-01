81074d8d2c0deb1476651c32faa2fdf3a7e4e58e
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-01 22:35:41 +0530

    Time: 0 ms (100.00%), Space: 12.9 MB (59.85%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,15 +1,31 @@
-class Solution { // TC is O(N) and SC is O(26)
+class Solution { 
 public:
-    int partitionString(string s) {
-        unordered_set<char>st;
-        int count = 1;
-        for(int i = 0;i<s.size();i++){
-            if(st.find(s[i])!=st.end()){
-                count++;
-                st.clear();
+    int partitionString(string s) { // TC is O(N) and SC is O(26) using the hashset
+        // unordered_set<char>st;
+        // int count = 1;
+        // for(int i = 0;i<s.size();i++){
+        //     if(st.find(s[i])!=st.end()){
+        //         count++;
+        //         st.clear();
+        //     }
+        //     st.insert(s[i]);
+        // }
+        // return count;
+        
+        // using the arrays with same tc and sc but it is faster compare to the hashset
+        
+        int partitioncount = 0;
+        int charCount[26]={0};
+        
+        for(char c:s){
+            if(charCount[c-'a']>0){
+                partitioncount++;
+                fill(begin(charCount), end(charCount), 0);
             }
-            st.insert(s[i]);
+            charCount[c-'a']++;
+            
         }
-        return count;
+        return partitioncount +1;
+        
     }
 };
