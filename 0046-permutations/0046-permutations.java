f4729d7798f72b7a1049ada14b345f06dded2c3c
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-01 23:37:56 +0530

    Time: 1 ms (94.16%), Space: 44.3 MB (69.14%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,32 @@
+class Solution { // Tc is O(n! * N) and Sc is o(N)
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        backtrack(resultList,new ArrayList<>(),nums);
+        return resultList;
+    }
+    
+    private void backtrack (List<List<Integer>> resultList,ArrayList<Integer> tempList,int[] nums){
+        
+        if(tempList.size() == nums.length){
+            resultList.add(new ArrayList<>(tempList));
+            return;
+        }
+        
+        for(int number : nums){
+            // skip if we get the same element
+            if(tempList.contains(number))
+                continue;
+            
+            // add the number
+            tempList.add(number);
+            
+            // backtrack again to checkthe other numbers
+            
+            backtrack(resultList,tempList,nums);
+            
+            // remove the number from the list
+            
+            tempList.remove(tempList.size()-1);
+        }
+    }
+}
