a96062b550da01d3bf80727584d2ca48c8941479
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-01 21:51:31 +0530

    Time: 8 ms (48.98%), Space: 42.1 MB (62.44%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<Integer> partitionLabels(String str) {
+    public List<Integer> partitionLabels(String str) { // Tc is O(n) and  SC is o(1)
         List<Integer> partitions = new ArrayList<>();
         // iterate through the allchar
         for(int i = 0 ;i<str.length();){
@@ -9,7 +9,7 @@ class Solution {
             
             // lop through the start and end index of the list
             for(int s = startIndex +1;s<= endIndex - 1;s++){
-                // we have to iterate through the last inedx of the same char
+                // we have to iterate through, find the last inedx of the same char
                 int lastIndexnextChar = str.lastIndexOf(str.charAt(s));
                 
                 if(lastIndexnextChar>endIndex){
