e30a683e3cbd3aba5b7e8a40d03a906379003b45
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-03 23:05:03 +0530

    Time: 1 ms (76.33%), Space: 41.8 MB (20.52%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,19 +1,46 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        return processString(s).equals(processString(t));
-    }
-    private String processString(String str){
-        StringBuilder result = new StringBuilder();
+    // public boolean backspaceCompare(String s, String t) {
+    //     return processString(s).equals(processString(t));
+    // }
+    
+    // using the stringbuilder techinque for this problem
+//     private String processString(String str){
+//         StringBuilder result = new StringBuilder();
         
+//         for(char c:str.toCharArray()){
+//             if(c == '#'){
+//                if(result.length()>0){
+//                    result.deleteCharAt(result.length()-1);
+//                }} else{
+//                    result.append(c);
+//                }
+            
+//         }
+     // return result.toString();// mehtod one
+    
+    public boolean backspaceCompare(String s, String t) {
+        return usingStack(s).equals(usingStack(t));
+    }
+    
+    
+    // using the stack method
+    
+    private String usingStack(String str){
+        Stack<Character> stack = new Stack<>();
         for(char c:str.toCharArray()){
             if(c == '#'){
-               if(result.length()>0){
-                   result.deleteCharAt(result.length()-1);
-               }} else{
-                   result.append(c);
-               }
-            
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            }else{
+                stack.push(c);
+            }
         }
-        return result.toString();
-    }
+        StringBuilder result = new StringBuilder();
+        while(!stack.isEmpty()){
+            result.append(stack.pop());
+        }
+        return result.reverse().toString(); // we have to do the revrse for stack 
+    }   
+        
 }
