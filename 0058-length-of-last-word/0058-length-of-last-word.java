167b3401f9114892ee3ca9c9b71851029af285f2
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-04 00:19:07 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (37.65%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,9 +1,30 @@
 class Solution {
-    public int lengthOfLastWord(String s) { // Tc is O(n) and Sc is O(1)
-        String[] words = s.split(" ");
+//     public int lengthOfLastWord(String s) { // Tc is O(n) and Sc is O(n)
+//         String[] words = s.split(" ");
         
-        String lastword = words[words.length-1];
+//         String lastword = words[words.length-1];
         
-        return lastword.length();
-    }
+//         return lastword.length();
+//     }
+    
+    // we cant use the extra space to store the string
+    
+     public int lengthOfLastWord(String s) {
+        int count = 0;
+         for(int i = s.length()-1;i>=0;i--){
+             // we dont have the white space
+             if(s.charAt(i) != ' '){
+                 count++;
+             }else{
+                 // we found the white space 
+                 // we are starting to find the word
+                 // yes really we got the word
+                 if(count>0) return count;
+             }
+         }
+         return count;
+         
+     }
+    
+    
 }
