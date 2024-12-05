4f37fe3e0d00ed9a517a754368eb8910dbfc6623
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-05 18:23:48 +0530

    Time: 1 ms (61.77%), Space: 41.9 MB (33.33%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,21 @@
+class Solution {// TC is O(N) and sc is O(M)
+    public int numJewelsInStones(String jewels, String stones) {
+        HashSet<Character> jewelset = new HashSet<>();
+        
+        // add the diffent jewels into the sets
+        
+        for(int i = 0;i<jewels.length();i++){
+            
+            jewelset.add(jewels.charAt(i));
+        }
+        int count = 0;
+        for(int i =0;i<stones.length();i++){
+            // checking the jewel in stone sets
+            if(jewelset.contains(stones.charAt(i))){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
