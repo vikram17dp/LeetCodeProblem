ee52d39663bd2c7faf848291bd8143d2114571e7
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-05 19:41:21 +0530

    Time: 11 ms (70.22%), Space: 42.7 MB (14.83%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,28 +1,25 @@
-class Solution {
+class Solution { 
     public boolean isIsomorphic(String s, String t) {
-        // base case 
+      // base case
         if(s.length()!=t.length()) return false;
-        HashMap<Character,Character> mapStoT = new HashMap<>();
-        HashMap<Character,Character> mapTtoS = new HashMap<>();
         
-        for(int i = 0;i<s.length();i++){
-            char charS = s.charAt(i);
-            char charT = t.charAt(i);
-            // checking for the first set
-            if(mapStoT.containsKey(charS)){
-                if(mapStoT.get(charS)!= charT)
-                    return false;
-                
-            }else{
-                mapStoT.put(charS,charT);
-            }
-            // checking for the second set
-            if(mapTtoS.containsKey(charT)){
-                if(mapTtoS.get(charT)!=charS){
-                    return false;
+        HashMap<Character,Character> charMap = new HashMap<>();
+        
+        for(int i =0;i<s.length();i++){
+            char original = s.charAt(i);
+            char replacment = t.charAt(i);
+            
+            if(!charMap.containsKey(original)){
+                if(!charMap.containsValue(replacment)){
+                   charMap.put(original,replacment);
+                }else{
+                     return false;
                 }
             }else{
-                mapTtoS.put(charT,charS);
+                char mappedChar = charMap.get(original);
+                if(mappedChar != replacment){
+                    return false;
+                }
             }
         }
         return true;
