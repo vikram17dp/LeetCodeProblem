c509ea0c88c4cd203cefbe777b826fe7d1f629f7
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-05 23:49:47 +0530

    Time: 1 ms (72.43%), Space: 44.8 MB (20.50%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -1,25 +1,14 @@
 class Solution {
     public String restoreString(String s, int[] indices) { // tc and sc is o(n)
         // // we have to store in the array of char
-        // char[] str = new char[s.length()];
+        char[] str = new char[s.length()];
         // // loop through the array of char
-        // for(int i = 0;i<s.length();i++){
-        //     str[indices[i]] = s.charAt(i);
-        // }
+        for(int i = 0;i<s.length();i++){
+            str[indices[i]] = s.charAt(i);
+        }
         // // return the string of that array
-        // return String.valueOf(str);
+        return String.valueOf(str);
         
-        HashMap<Integer,Character> map = new HashMap<>(); // tc and sc is o(n)
-        
-        for(int i =0;i<s.length();i++){
-            map.put(indices[i],s.charAt(i));
-        }
-        
-        StringBuilder result = new StringBuilder(s.length());
-        
-        for(int i =0;i<s.length();i++){
-            result.append(map.get(i));
-        }
-        return result.toString();
+       
     }
 }
