959aa6708c96ab978966716041ce4c3e119843db
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-08 21:46:21 +0530

    Time: 10 ms (89.09%), Space: 58.3 MB (35.93%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,14 +1,16 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) { // O(n)
-        HashSet<Integer> visited = new HashSet<>();
-        for(int i =0;i<nums.length;i++){
-            if(visited.contains(nums[i])){
-                return true;
-            }else{
-                visited.add(nums[i]);
-            }
-        }
-        return false;
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>();
         
+        for(int i =0;i<n;i++){
+            
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+        }
+       
+        return false;
     }
 }
