0cb44c6950ff2b5836d526354e166295a3d9ad2f
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-18 22:45:05 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (38.92%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,11 +1,18 @@
-class Solution {
+class Solution { // tc is O(sqrt(num)) and sc is O(1)
     public boolean isPerfectSquare(int num) {
-        
-        for(long i = 1;i*i<=num;i++){
-            if(num == i*i){
-                return true;
-            }
-        }
+        if(num < 1) return false;
+       long  low = 1,high = num;
+       while(low<=high){
+           long mid = low + (high-low)/2;
+           long sqrtnum = mid*mid;
+           if(sqrtnum == num ){
+               return true;
+           }else if(sqrtnum<num){
+              low = mid+1;
+           }else{
+               high = mid-1;
+           }
+       }
         return false;
     }
 }
