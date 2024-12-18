fa1e01d8ffbed333074896f6db10b4611a40fb5a
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-18 22:54:36 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (32.04%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,19 @@
+class Solution {// tc is O(log(num)) and sc is O(1)
+public:
+    bool isPerfectSquare(int num) {
+         if(num < 1) return false;
+       long  low = 1,high = num;
+       while(low<=high){
+           long mid = low + (high-low)/2;
+           long sqrtnum = mid*mid;
+           if(sqrtnum == num ){
+               return true;
+           }else if(sqrtnum<num){
+              low = mid+1;
+           }else{
+               high = mid-1;
+           }
+       }
+        return false;
+    }
+};
