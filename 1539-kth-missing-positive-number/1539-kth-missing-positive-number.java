4c0e7956b7c700e51a823f8cc509099ae7c9c5c5
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-19 21:49:15 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (10.17%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -3,6 +3,7 @@ class Solution { // tc is O(n) and sc is O(1) and optimal sloution
         int low = 0,high = arr.length-1;
         while(low<=high){
             int mid = low + (high-low)/2;
+            // it tells about the previous missing numbers
             int missing = arr[mid]-(mid+1);
             if(missing < k){
                 low = mid+1;
