6b90fd0e7d3dab172aa65319a86060e8d5b6018d
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-20 22:25:47 +0530

    Time: 3 ms (86.86%), Space: 46.7 MB (61.99%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,20 +1,20 @@
-class Solution { // tc is O(nsquare) and sc is O(1)
+class Solution { // tc is O(n) and sc is O(1)
     public int longestOnes(int[] nums, int k) {
-        int n = nums.length,maxLength = 0;
+        int n = nums.length,maxLength = 0 ,right = 0,left =0,zeroCount = 0;
         
-        for(int i =0;i<n;i++){
-             int zeroCount = 0;
-            for(int j = i;j<n;j++){
-               
-                if(nums[j] == 0){
-                    zeroCount++;
-                }
-                if(zeroCount <= k){
-                    maxLength = Math.max(maxLength,j-i+1);
-                }else{
-                    break;
-                }
+        while(right < n){
+            if(nums[right] == 0){
+                zeroCount++;
             }
+            
+            while(zeroCount > k){
+                if(nums[left] == 0){
+                    zeroCount--;
+                }
+                left++;
+            }
+            maxLength = Math.max(maxLength, right - left + 1);
+            right++;
         }
         return maxLength;
     }
