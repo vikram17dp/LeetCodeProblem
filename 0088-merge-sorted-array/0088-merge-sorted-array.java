45dbc4004febf3aec6a75123367855d94e0e8d1e
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-23 21:58:45 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (92.78%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,34 +1,15 @@
-class Solution { // tc is O(n+m)+O(n+m) and sc is O(n+m)
+class Solution { // tc is O(n+m) and sc is O(1)
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] nums3 = new int[n+m];
-        int left = 0,right = 0,index = 0;
-        // merging step
-        while(left <m && right <n){
-            if(nums1[left] <= nums2[right]){
-                nums3[index] = nums1[left];      
-                left++;
-                index++;
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        
+        while(j>=0){
+            if(i>=0 && nums1[i]>nums2[j]){
+                nums1[k--]= nums1[i--];
             }else{
-                nums3[index] = nums2[right];
-                right++;
-                index++;
+                nums1[k--] = nums2[j--];
             }
-        }
-        
-        // extra element in left
-        while(left < m){
-            nums3[index++] = nums1[left++];
-        }
-        // extra element in right
-        
-        while(right < n){
-            nums3[index++] = nums2[right++];
-        }
-        
-        // store back the ele into original arr
-        
-        for(int i = 0;i<m+n;i++){
-            nums1[i] = nums3[i];    
         }
     }
 }
