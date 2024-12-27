b150e83e6a8da9c8451fa698cf4dc8dcd4d4e00b
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2024-12-27 12:05:33 +0530

    Time: 1 ms (100.00%), Space: 44.5 MB (57.97%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,4 +1,4 @@
-class Solution { // tc is O(n) and sc is O(1)
+class Solution { // tc is O(n) and sc is O(101) = o(n)
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int n = nums.length;
       
@@ -9,19 +9,16 @@ class Solution { // tc is O(n) and sc is O(1)
             buckets[num]++;
         }
         
-        // get buckets[i] += buckets[i-1]
+        // get buckets[i] += buckets[i-1] and comput the prefix sum
         
         for(int i = 1;i<buckets.length;i++){
             buckets[i] += buckets[i-1];
         }
         
         int[] result = new int[n];
+        // Map the result using the prefix sums
         for(int i = 0;i<result.length;i++){
-            if(nums[i] == 0){
-                result[i] = 0;
-            }else{
-                result[i] = buckets[nums[i]-1];
-            }
+             result[i]=nums[i] == 0 ? result[i] = 0: buckets[nums[i]-1];
         }
         
         return result;
