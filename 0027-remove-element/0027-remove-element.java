8367dd8ff2403be54df44d4f469161fb6b962cee
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-01-02 20:04:04 +0530

    Update 0027-remove-element.java

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,6 +3,7 @@ class Solution {
         int ptr=0;
         for(int i=0;i<nums.length;i++){
 // if the nums[i] = val then it will consider as flase then it will comes out if not eqaul then it will return the nums[i] in ana array
+
             if(nums[i]!=val){
                 nums[ptr] = nums[i];
                 ptr++;
