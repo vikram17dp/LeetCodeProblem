31ef1e399c5d6c134f1f82a36c1f475c7754b1ac
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-01-03 19:34:31 +0530

    Update 0053-maximum-subarray.java

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -2,6 +2,7 @@ class Solution { // TC is O(n) and SC is O(1)
     public int maxSubArray(int[] nums) {
         int maxsum =nums[0];
         int currentsum = nums[0];
+
         if(nums.length == 0) return 0;
         for(int i =1;i<nums.length;i++){
             currentsum = Math.max(nums[i],currentsum+nums[i]);
