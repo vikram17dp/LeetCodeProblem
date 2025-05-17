1e595166d9c1414bccf242e83067660733714382
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-05-17 18:48:45 +0530

    Time: 0 ms (100%), Space: 42.5 MB (27.52%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -13,20 +13,22 @@
  *     }
  * }
  */
-class Solution { // tc is O(n) and sc is O(N)
+class Solution { // tc is O(n) and sc is O(1) and morris traversal
     public void flatten(TreeNode root) {
-        if(root == null) return;
-        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
 
-        stack.push(root);
-
-        while(!stack.isEmpty()){
-            TreeNode curr = stack.pop();
-
-            if(curr.right != null) stack.push(curr.right);
-            if(curr.left != null) stack.push(curr.left);
-            if(!stack.isEmpty()) curr.right = stack.peek();
-            curr.left = null;
+        while(curr != null){
+            if(curr.left != null){
+                TreeNode prev = curr.left;
+                while(prev.right != null){
+                    prev = prev.right;
+                }
+                prev.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+            }
+            curr = curr.right;
         }
+        
     }
 }
