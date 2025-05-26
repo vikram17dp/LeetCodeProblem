062ed2bb2c0734f68977282b4829e610e0c50814
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-05-26 17:49:24 +0530

    Update 0028-find-the-index-of-the-first-occurrence-in-a-string.java

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -7,6 +7,7 @@ class Solution { // tc is o(n) and sc is O(1)
         for(int i = 0;i<=m-n;i++){
             if(haystack.substring(i,i+n).equals(needle)){
                 return i;
+
             }
         }
         return -1;
