9d24e3ec87dd19957782e3ad1b22910a78bd9cf7
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-05-28 09:04:41 +0530

    Time: 2 ms (91.03%), Space: 41.2 MB (74.23%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,21 @@
+class Solution { // tc and sc is O(2ⁿ)
+    public String countAndSay(int n) {
+        if(n == 1) return "1";
+
+        String prev = countAndSay(n-1);
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+        for(int i = 1;i< prev.length();i++){
+            if(prev.charAt(i) == prev.charAt(i-1)){
+                count++;
+            }else{
+                result.append(count).append(prev.charAt(i-1));
+                count = 1;
+            }
+        }
+        // apepend the last group
+        result.append(count).append(prev.charAt(prev.length()-1));
+
+        return result.toString();
+    }
+}
