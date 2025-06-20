4f91efbe225a12bbe07ee77d69dc80c0533da3c2
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-06-20 17:34:17 +0530

    Time: 14 ms (29.39%), Space: 44.6 MB (49.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,35 +1,21 @@
 class Solution {
-    public boolean isAnagram(String str1, String str2) { // TC is O(N) and Sc is O(1)
-        // converting all the strings into the lowercase
-        str1 = str1.toLowerCase();
+    public boolean isAnagram(String str1, String str2) { 
+        if(str1.length() != str2.length()) return false;
+        
+        Map<Character,Integer> map = new HashMap<>();
 
-        str2 = str2.toLowerCase();
-        
-        // removing the whitespaces
-        str1 = str1.replace(" ","");
-        str2 = str2.replace(" ","");
-        
-        // intailze the bucket array of 26 
-        
-        int[] counts = new int[26];
-        
-        //fill the bucktes
-        for(int i = 0;i<str1.length();i++){
-            counts[str1.charAt(i)-'a']++;
+        for(char c: str1.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
         }
-        
-        // empty the buckets
-        for(int i = 0;i<str2.length();i++){
-            counts[str2.charAt(i)-'a']--;
+
+        for(char c: str2.toCharArray()){
+            if(!map.containsKey(c)) return false;
+                map.put(c,map.get(c)-1);
+                if(map.get(c) == 0) map.remove(c);   
         }
-        // apply the for each loop to travse entrie array
-        
-        for(int count:counts){
-            if(count!=0){
-                return false;
-            }
-            
-        }
-        return true;
+    
+
+       
+        return map.isEmpty();
     }
 }
