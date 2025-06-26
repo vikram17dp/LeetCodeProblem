c2676be2971348740b593803e62ae8b6d4e49ddf
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-06-26 21:52:13 +0530

    Time: 0 ms (100%), Space: 41.5 MB (28.11%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,15 +1,11 @@
 class Solution {
     public boolean check(int[] nums) { // O(N) and O(1)
         int countbreak = 0;
-        for(int i = 0 ;i<nums.length;i++){
-            
+        for(int i = 0;i<nums.length;i++){
             if(nums[i] > nums[(i+1) % nums.length]) countbreak++;
-            
-            // 5%5 is equal to 0
-            
-            if(countbreak> 1) return false;
-            
+
+            if(countbreak > 1) return false;
         }
         return true;
-    }
+    } 
 }
