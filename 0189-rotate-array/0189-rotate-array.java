6fcf51c1ac78e4caf3136624854f87c8a91b97cd
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-06-27 19:54:24 +0530

    Time: 0 ms (100%), Space: 57.1 MB (85.73%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -2,21 +2,22 @@ class Solution { // O(N) & O(1)
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k % n;
-        // step1 reverse full array
+        
+        // step1 : reverse the owl array
         reverse(nums,0,n-1);
-        // step2 reverse the k numbers of arrays
+        // step2: reverse the k elements in an array
         reverse(nums,0,k-1);
-        // step3 reverse the index from k to n-1 arrays
+        // step3: reverse the n-k arrays means remaining arrays
         reverse(nums,k,n-1);
     }
-    private void reverse(int[] nums,int start,int end){
-            while(start<end){
-                int temp = nums[start];
-                nums[start] = nums[end];
-                nums[end] = temp;
-                start++;
-                end--;
-            }
+    public void reverse(int[] nums,int start,int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
+    }
 
 }
