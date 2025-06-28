371e99d768d38378dc8715763c8f2b3872a5c073
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-06-28 22:03:05 +0530

    Time: 0 ms (100%), Space: 45.6 MB (21.58%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,15 +1,18 @@
-class Solution {
+class Solution { // bitwise method is xor approach 1 ^1 = 0, 1^0 = 1;
     public int missingNumber(int[] nums) { // TC is O(N) and SC is O(1)
         int n = nums.length;
         
-        int expectedSum = n * (n+1)/2;
-        int actualSum = 0;
-
-        for(int num:nums){
-            actualSum += num;
+        int xor = 0;
+        // first loop is store from o to n numbers
+        for(int i = 0;i<=n;i++){
+            xor ^= i;
         }
+        // second loop just taken only the elements whatever in the nums
+        for(int num:nums){
+            xor ^= num;
+        }
+        return xor;
 
-        return expectedSum - actualSum;
     }
     
 }
