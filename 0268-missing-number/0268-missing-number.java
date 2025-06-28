9f2b32c53870df1de278d1ae7e455304db9547a0
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-06-28 21:45:52 +0530

    Time: 0 ms (100%), Space: 45.7 MB (21.58%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,25 +1,15 @@
 class Solution {
     public int missingNumber(int[] nums) { // TC is O(N) and SC is O(1)
         int n = nums.length;
-        // optimal 1
-//         int sum = n*(n+1)/2;
-//         int s2 = 0;
         
-//         for(int i = 0 ; i<n ; i++){
-//             s2 += nums[i];
-//         }
-//         return (sum-s2);
-        
-// optimal 2
-        int xor1 = 0;
-        int xor2 = 0;
-        for(int i=0;i<=n;i++){
-           xor1 =  xor1^i;
+        int expectedSum = n * (n+1)/2;
+        int actualSum = 0;
+
+        for(int num:nums){
+            actualSum += num;
         }
-        for(int i = 0;i<n;i++){
-          xor2 =  xor2^nums[i];
-        }
-        return xor1^xor2;
+
+        return expectedSum - actualSum;
     }
     
 }
