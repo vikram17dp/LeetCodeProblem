30d03666b28f5ca8b059ba4b3325776ee49a713c
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-08-13 19:39:48 +0530

    Time: 24 ms (70.29%), Space: 57.7 MB (68.3%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -0,0 +1,25 @@
+class Solution {// tc is O(n) and sc is O(n) and prefix sum modulo + hashmap
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int prefixSum = 0;
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        map.put(0,-1);// for oth index having -1, then actualyy when we are doing we can consider an oth index as well 
+
+        for(int i = 0;i<n;i++){
+            prefixSum += nums[i];
+
+            int rem = prefixSum % k;
+            if(rem < 0) rem += k;// to make an +ve
+            if(map.containsKey(rem)){
+                if(i - map.get(rem) >= 2){
+                    return true;
+                }
+            }else{
+                map.put(rem,i);
+            }
+        }
+        return false;
+    }
+}
