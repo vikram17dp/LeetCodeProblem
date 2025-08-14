f333726a0d05711684c6fbc08d282f2435a799f5
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-08-15 00:54:24 +0530

    Time: 30 ms (70.26%), Space: 51.7 MB (47.57%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,42 @@
-class Solution {
+class Solution {// two pointer approach left and right and tc is O(nsqaure) and sc is O(1) or O(n)
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
-        for(int i=0;i<nums.length&& nums[i]<=0;i++){
-            if(i == 0|| nums[i] != nums[i-1]){
-                twoSum2(nums,i,result);
-                
-            }
-        }
-        return result;
-    }
-    void twoSum2(int[] nums,int i,List<List<Integer>> result){
-        int left = i+1;
-        int right = nums.length-1;
-        while(left<right){
-            int sum = nums[i] + nums[left] + nums[right];
-            if(sum<0){
+       List<List<Integer>> result = new ArrayList<>();
+
+       int n = nums.length;
+
+       // sort
+       Arrays.sort(nums);// tc is O(nlogn)
+
+       for(int i = 0;i<n-2;i++){// tc is O(n)
+            if(i > 0 && nums[i] == nums[i-1]) continue;// for the duplicates elements
+            int left = i + 1;
+            int right = n - 1;
+
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum == 0){// tc is O(n)
+                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+
+                // skip the duplicates from left values
+                while(left < right && nums[left] == nums[left + 1]) left++;
+
+                // skip the duplicates from right values
+                while(left < right && nums[right] == nums[right -1]) right--;
+                // if both the left and right next values are not equal means
                 left++;
-            }else if(sum>0){
                 right--;
-            }else{
-               result.add(Arrays.asList(nums[i],nums[left++],nums[right--]));
-                while(left<right && nums[left]==nums[left-1]){
-                    ++left;
                 }
+            
+            else if(sum < 0){
+                left++;
+            }else{
+                right--;
             }
-        }
+            }
+       }
+       return result;
     }
+   
     
 }
