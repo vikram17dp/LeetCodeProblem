c1a8eca18466597adb5f724abf93249e3832551e
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-08-15 19:20:40 +0530

    Time: 11 ms (11.49%), Space: 46.5 MB (85.74%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,26 +1,31 @@
-class Solution {
+class Solution {// tc is O(nlogn) and sc is O(n) Sorting + Two Pointers
     public int[][] merge(int[][] intervals) {
         // base case
-        if(intervals.length<=1){
-            return intervals;
-        } 
-        // it will sort a/c to first vlaue of intervals
-        Arrays.sort(intervals,Comparator.comparingInt(i->i[0]));
-        
-        ArrayList<int[]> result = new ArrayList<>();
-        
-        int[] newIntervals = intervals[0];
-        result.add(newIntervals);
-        
-        for(int[] interval : intervals){
-            if(interval[0] <= newIntervals[1]){
-                newIntervals[1] = Math.max(newIntervals[1],interval[1]);
+        if(intervals.length <= 1) return intervals;
+
+
+        // sort the intervals based on the 0th indexed only
+        //[1,3] arrays means [0,1] style it is saved
+
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+
+        List<int[]> result = new ArrayList<>();
+
+        int[] current = intervals[0];
+
+        for(int i = 1;i<intervals.length;i++){
+            if(intervals[i][0] <= current[1]){
+                current[1] = Math.max(current[1],intervals[i][1]);
             }else{
-                newIntervals = interval;
-                result.add(newIntervals);
+                // add to the result;
+                result.add(current);
+                current = intervals[i];
             }
         }
-        
+            // add the last intervals
+            result.add(current);
+
         return result.toArray(new int[result.size()][]);
+        
     }
 }
