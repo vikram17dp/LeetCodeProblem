b1f7078f3779ebd90e491675203c0be4254b0efe
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-08-16 00:41:09 +0530

    Time: 2 ms (54.89%), Space: 44.8 MB (73.09%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,18 +1,16 @@
 class Solution { // tc is O(n) and sc is O(1)
     public int maxProduct(int[] nums) {
-        int min  = nums[0],max = nums[0],n = nums.length,result = max;
-      
-        for(int i = 1;i<n;i++){
+       
+       int max = nums[0],min = nums[0],n = nums.length,result = max;
+
+       for(int i = 1;i<n;i++){
             int curr = nums[i];
-            
-            int newMax = Math.max(curr,Math.max(max*curr,min*curr));
+            int newMax = Math.max(curr,Math.max(max * curr,min*curr));
             min = Math.min(curr,Math.min(max*curr,min*curr));
             max = newMax;
-            result = Math.max(result,max);
-        }
-        return result;
-        
-        
-       
+
+            result = Math.max(max,result);
+       }
+       return result;
     }
 }
