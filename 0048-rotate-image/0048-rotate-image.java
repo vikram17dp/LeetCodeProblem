0a270d9b2dbed413adc6a0356a9d535e7dda75af
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-08-16 10:34:19 +0530

    Time: 0 ms (100%), Space: 42 MB (89.69%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -3,17 +3,16 @@ class Solution {
         
         int n = matrix.length;
         // step 1=> transpose the matrix
-        for(int i =0; i<n ;i++){
-            for(int j =i; j<n;j++){
+        for(int i = 0;i<n;i++){
+            for(int j = i;j<n;j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+                matrix[j][i]=temp;
             }
         }
-
         // step 2 => reverse the matrix using swaping techinque
         for(int i = 0;i<n;i++){
-            int left = 0,right = n-1;
+        int left = 0,right =  n - 1;
             while(left<right){
                 int temp = matrix[i][left];
                 matrix[i][left] = matrix[i][right];
