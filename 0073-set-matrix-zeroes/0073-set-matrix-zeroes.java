9516513711fd366618a88c088d0097782047b7a6
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-08-16 10:00:20 +0530

    Time: 1 ms (72.46%), Space: 45.6 MB (67.11%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,28 +1,28 @@
 class Solution { 
-    public void setZeroes(int[][] matrix) { // tc is O(n*m) * O(n*m)
+    public void setZeroes(int[][] matrix) { // tc is O(n*m) * O(n*m) and sc is o(n) + O(m)
         int m = matrix.length;
         int n = matrix[0].length;
-        
+
         int[] row = new int[m];
         int[] col = new int[n];
-        
+
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
                 if(matrix[i][j] == 0){
-                   row[i]=1;
-                    col[j]= 1;
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
+        // then travse to the col and row array
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
-                if(row[i] == 1 || col[j] == 1){
+                if(row[i]== 1 || col[j] == 1){
                     matrix[i][j] = 0;
                 }
             }
         }
+    
+    
     }
-    
-    
-    
 }
