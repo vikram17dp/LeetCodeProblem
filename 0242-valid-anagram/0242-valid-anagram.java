b75e57fdbb256f3782d1329bb76d110d0e64f21c
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2025-08-17 12:02:45 +0530

    Time: 1 ms (99.58%), Space: 43.6 MB (61.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,38 @@
-class Solution {
+class Solution { // tc is O(n) and sc is O(1)
     public boolean isAnagram(String str1, String str2) { 
         if(str1.length() != str2.length()) return false;
         
-        Map<Character,Integer> map = new HashMap<>();
+        // Map<Character,Integer> map = new HashMap<>();
 
-        for(char c: str1.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
-        }
+        // for(char c: str1.toCharArray()){
+        //     map.put(c,map.getOrDefault(c,0)+1);
+        // }
 
-        for(char c: str2.toCharArray()){
-            if(!map.containsKey(c)) return false;
-                map.put(c,map.get(c)-1);
-                if(map.get(c) == 0) map.remove(c);   
-        }
+        // for(char c: str2.toCharArray()){
+        //     if(!map.containsKey(c)) return false;
+        //         map.put(c,map.get(c)-1);
+        //         if(map.get(c) == 0) map.remove(c);   
+        // }
     
 
        
-        return map.isEmpty();
+        // return map.isEmpty();
+
+        // second approach tc is O(n) and sc is O(1) it will store 26 letters
+
+        int[] count = new int[26];
+
+        for(char c:str1.toCharArray()){
+            count[c-'a']++;
+        }
+        for(char c:str2.toCharArray()){
+            count[c-'a']--;
+        }
+        for(int val:count){
+            if(val != 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
