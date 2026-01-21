a18b1077dbbe66104307208f332b46d703efa95b
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2026-01-21 16:29:25 +0530

    Time: 3 ms (84.81%), Space: 69.6 MB (51.3%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,24 +1,22 @@
 class Solution { // tc is O(n) and sc is O(1)
     public double findMaxAverage(int[] nums, int k) {
         int n = nums.length;
-        int startIdx = 0,endIdx=k,sum =0;
-        for(int i= 0;i<k;i++)
-            sum += nums[i];
-        
-        int maxAvg = sum;
-        
-        while(endIdx < n){
-            // remove the start element
-            sum -= nums[startIdx];
-            startIdx++;
-            
-            // add the new element
-            sum += nums[endIdx];
-            endIdx++;
-            
-            maxAvg = Math.max(maxAvg,sum);
-            
+
+        int windowSum = 0;
+
+        for(int i = 0;i<k;i++){
+            windowSum += nums[i];
         }
+
+        int maxAvg = windowSum;
+
+        for(int end = k;end<n;end++){
+            windowSum += nums[end];// adding the next element
+            windowSum -= nums[end-k]; // remove the previous element
+            maxAvg = Math.max(maxAvg,windowSum);
+        }
+        
+        
         return (double) maxAvg / k;
     }
 }
