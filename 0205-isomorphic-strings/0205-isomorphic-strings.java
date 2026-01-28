79aa2332fda3bf8725fbae6680b1b04556da12f3
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2026-01-28 17:58:38 +0530

    Time: 11 ms (63.6%), Space: 43.7 MB (81.07%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,11 +1,10 @@
-class Solution { // tc is O(n) and sc O(26 or 128) 26-english letters and other Asiic char
+class Solution { // tc is O(n) and sc O(26 or 256) 26-english letters and other Asiic char
     public boolean isIsomorphic(String s, String t) {
       // base case
         if(s.length()!=t.length()) return false;
         
         HashMap<Character,Character> charMap = new HashMap<>();
         
-
         for(int i =0;i<s.length();i++){
             char original = s.charAt(i);
             char replacment = t.charAt(i);
