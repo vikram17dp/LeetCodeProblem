b5670a34f96bbed9905d6e7585cc4225c59ad545
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2026-09-01 20:53:00 +0530

    Time: 0 ms (100%), Space: 42 MB (73.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,16 @@
 class Solution {// tc is O(n) and sc is O(1)
     public int climbStairs(int n) {
-        if(n == 1) return 1;
-        if(n == 2) return 2;
-        
-        int prev1 = 2;
-        int prev2 = 1;
-
+        // if(n == 1) return 1;
+        // if(n == 2) return 2;
+        if(n <= 2) return n;
+        int prev1 = 1;
+        int prev2 = 2;
         
         for(int i = 3;i<=n;i++){
             int current = prev1+prev2;
-            prev2 = prev1;
-            prev1 = current;
+            prev1 = prev2;
+            prev2 = current;
         }
-        return prev1;
+        return prev2;
     }
 }
