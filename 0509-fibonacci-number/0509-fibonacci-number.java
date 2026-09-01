f19d9973bfa1c0868e712bd079c52301c653e67c
Vikram D P <144223883+vikram17dp@users.noreply.github.com>
2026-09-01 20:50:21 +0530

    Time: 0 ms (100%), Space: 42 MB (70.41%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,18 +1,19 @@
 class Solution { // tc is o(n) and sc is O(1)
     public int fib(int n) {
-        if(n == 0){
-           return 0;
-       } else if(n == 1){
-           return 1;
-       }
+    //     if(n == 0){
+    //        return 0;
+    //    } else if(n == 1){
+    //        return 1;
+    //    }
+    if(n <= 1) return n;
        // FT = FirstTerm like that for all same only
         int FT=0;
         int ST=1;
-        for(int i=1;i<=n;i++){
+        for(int i=2;i<=n;i++){
             int TT = FT + ST;
             FT = ST;
             ST = TT;
         }
-        return FT;
+        return ST;
     }
 }
